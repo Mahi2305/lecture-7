1877ade70bd8aa4c92546549a4202976abd3cd92
Mahi2305 <114794143+Mahi2305@users.noreply.github.com>
2024-07-02 12:49:17 +0530

    Add files via upload

---
+++ lecture 7 - arrays.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a[5]; // declaration of an array
+	// array stores data of the same type
+	// array name should be similar to variable name
+	// a[5] contains 6 items as indexing starts from 0 and not 1.
+	
+}
